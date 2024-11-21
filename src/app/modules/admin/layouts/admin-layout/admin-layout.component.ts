f386f2054eb0be75b497e8ae18be394b980f0ccd
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-21 17:08:33 +0100

    wip

--- src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
+++ src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuComponent } from "../../../shared/components/side-menu/side-menu.component";
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenuComponent],
   templateUrl: './admin-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
