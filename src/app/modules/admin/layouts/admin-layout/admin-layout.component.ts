ad586d97f8644df410772509f464b7fc93856576
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-22 23:53:31 +0100

    usage of component in NPM

--- src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
+++ src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
@@ -1,16 +1,18 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideMenuComponent } from "../../../shared/components/side-menu/side-menu.component";
+import { FrhSideMenuComponent, TitleColor } from 'frh-side-menu';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [RouterOutlet, SideMenuComponent],
+  imports: [RouterOutlet, SideMenuComponent, FrhSideMenuComponent],
   templateUrl: './admin-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class AdminLayoutComponent
 {
+  TitleColor = TitleColor;
   isAuthenticated =  signal(false);
 
   onLogin() {
